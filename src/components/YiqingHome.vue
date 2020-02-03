351d5296f795e4bbb5ce29c8bbde8d7541add41b
solutionbasewang <17781297@qq.com>
2020-02-03 12:08:59 +0800

    frist

--- src/components/YiqingHome.vue
+++ src/components/YiqingHome.vue
@@ -1,11 +1,26 @@
 <template>
-    <div></div>
+  <div>
+    <el-tabs v-model="activeName" @tab-click="handleClick">
+      <el-tab-pane label="当日疫情" name="first">1</el-tab-pane>
+      <el-tab-pane label="要闻信息" name="second">2</el-tab-pane>
+      <el-tab-pane label="疫情地图" name="third">3</el-tab-pane>
+    </el-tabs>
+  </div>
 </template>
 <style scoped>
-
 </style>
 <script>
 export default {
-  name: 'yiqinghome'
+  name: 'yiqinghome',
+  data () {
+    return {
+      activeName: 'second'
+    }
+  },
+  methods: {
+    handleClick (tab, event) {
+      console.log(tab, event)
+    }
+  }
 }
 </script>
