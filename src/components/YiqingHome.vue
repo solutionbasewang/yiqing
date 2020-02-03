ba4b43f384024a14998a5c740dd19482607bdc00
solutionbasewang <17781297@qq.com>
2020-02-03 16:19:05 +0800

    sencond

--- src/components/YiqingHome.vue
+++ src/components/YiqingHome.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <el-tabs v-model="activeName" @tab-click="handleClick">
-      <el-tab-pane label="当日疫情" name="first">1</el-tab-pane>
-      <el-tab-pane label="要闻信息" name="second">2</el-tab-pane>
-      <el-tab-pane label="疫情地图" name="third">3</el-tab-pane>
+      <el-tab-pane label="当日疫情1" name="first">1</el-tab-pane>
+      <el-tab-pane label="要闻信息2" name="second">2</el-tab-pane>
+      <el-tab-pane label="疫情地图3" name="third">3</el-tab-pane>
     </el-tabs>
   </div>
 </template>
